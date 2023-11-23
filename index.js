7ffc39ccf0f8fe6c00114572909778a3c50e003b
Jason Warren <contact.jwarren@gmail.com>
2023-11-23 20:57:10 +0000

    Initial Tracery setup

---
+++ index.js
@@ -0,0 +1,10 @@
+//Discord API Stuff Goes Here
+
+//Jason's Stuff
+
+//Compromise: Language Input
+//Tracery: Language Output
+	/*
+		Repo at https://github.com/galaxykate/tracery/tree/tracery2
+	*/
+let grammar = tracery.createGrammar(language-output/example.json);//grammar.json
