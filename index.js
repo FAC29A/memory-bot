bd056fd514fa049b429ed38be33f2f941b052792
Jason Warren <contact.jwarren@gmail.com>
2023-11-25 20:26:13 +0000

    Created code to initialise bot with my .env file

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-/* IMPORT MODULES */
+/* IMPORTS */
+// Local Files
 require('dotenv').config();
 
-const grammar = require('./language/grammar.js');
+// Language Files
 const tracery = require('tracery-grammar');
 const nlp = require('compromise');
+const grammar = require('./language/grammar.js');
 
-/* DISCORD.JS */
+// Discord Files
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { DISCORD_TOKEN } = require('./.env');
 
 
-/* TRACERY: Language Output */
-
-/* let grammarExample = tracery.createGrammar(grammar.example);
-let outputExample = grammarExample.flatten("#origin#");
-console.log(outputExample); */
-
+/* LANGUAGE */
+// Tracery: Language Output
 const grammarFac = tracery.createGrammar(grammar.fac29a);
 const outputFac = grammarFac.flatten('#gossip#');
 
@@ -22,7 +22,7 @@ console.log('Gossip: ' + outputFac);
 console.groupEnd();
 
 
-/* COMPROMISE: Language Input */
+// Compromise: Language Input
 const input = nlp('We are building a Discord bot that\'ll make things easier.');
 
 console.groupCollapsed('Compromise Tests');
@@ -33,3 +33,18 @@ input.nouns().toPlural();
 console.log('Plural: ' + input.text());
 console.log('Nouns: ' + input.match('#Noun').text());
 console.groupEnd();
+
+
+/* BOT ARCHITECTURE */
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(DISCORD_TOKEN);
